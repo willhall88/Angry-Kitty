8af07f5dc92abf091511d632bbaa2ed97e271bb8
Will Hall <willhall88@hotmail.com>
2014-06-09 20:23:38 +0100

    added logic and tests for determining whether to send an email or not on the scheduling

--- lib/tasks/scheduler.rb
+++ lib/tasks/scheduler.rb
@@ -1,3 +1,16 @@
-def scheduler
 
+def send_mail?(user)
+  if user.maturity <= 7
+    true
+  elsif user.maturity <= 30 && user.last_harassed > 3
+    true
+  elsif user.maturity <= 60 && user.last_harassed > 7
+    true
+  elsif user.maturity <= 120 && user.last_harassed > 14
+    true
+  elsif user.maturity >= 120 && user.last_harassed > 30
+    true
+  else
+    false
   end
+end
