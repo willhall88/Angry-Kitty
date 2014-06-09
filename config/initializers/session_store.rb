f37028631afe3f8735de3c56b091a441543b70da
Nico Saueressig <nico@nicosaueressig.de>
2014-06-09 09:44:38 +0100

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Angry_Kitty_session'
+Rails.application.config.session_store :cookie_store, key: '_angry_kitty_session'
