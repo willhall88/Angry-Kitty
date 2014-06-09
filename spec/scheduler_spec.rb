d553041465ee26f094791b44ef502ae703ce1c10
Will Hall <willhall88@hotmail.com>
2014-06-09 18:35:37 +0100

    created scheduler specs

---
+++ spec/scheduler_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require_relative '../lib/tasks/scheduler'
+
+describe "Scheduler" do
+
+  before do
+    user = build(:user, paid: false)
+  end
+
+  it '' do
+    
+
+  end
+
+end
