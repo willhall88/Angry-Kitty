ec6b500a58192676944dc83329d7722848ee6d55
Robin <robindoble@gmail.com>
2014-06-11 17:15:31 +0100

    show action for events controller queries debts table for paid and unpaid participants

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,9 @@ class EventsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
+		#where returns an array (needed for the each iterator in view)
 		@organising = Event.where(organiser: current_user)
+		#need to check below does not include organisers (might need to be current_user.debts)
 		@participating = current_user.events
 	end
 
@@ -13,7 +15,6 @@ class EventsController < ApplicationController
 	end
 
 	def create
-		# raise params.inspect
 		@event = Event.new(params[:event].permit(:title, :description, :deadline, :total, :angerlevel, userinvitees_attributes: [:name, :mobile, :email]))
 		@event.organiser = current_user
 		@event.save
@@ -22,8 +23,9 @@ class EventsController < ApplicationController
 
 	def show
 		@event = Event.find(params[:id])
-		@users = @event.users
-		# @userinvitees = Userinvitee.all
-		@userinvitees = @event.userinvitees
+		#below is empty until userinvitees have confirmed participation
+		#@event.users does not work, we need to querey the debt table directly
+		@paid = Debt.where(event: @event).where(paid: true)
+		@unpaid = Debt.where(event: @event).where(paid: nil)
 	end
 end
