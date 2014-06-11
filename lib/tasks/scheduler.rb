7f3c853c6ff7a1c770499b8a7f7f24807a57b60f
Will Hall <willhall88@hotmail.com>
2014-06-11 20:05:05 +0100

    fixed the last harrassed on the take task

--- lib/tasks/scheduler.rb
+++ lib/tasks/scheduler.rb
@@ -7,26 +7,24 @@ ANGER_CHART = {
 
 def send_harassment
   Debt.unpaid.each do |debt|
-    debt.harass! if send_mail?(debt.deadline, debt.last_harassed, debt.event.angerlevel)
-    debt.last_harassed = time_now
-    debt.save!
+    if send_mail?(debt.deadline, debt.last_harassed, debt.event.angerlevel)
+      debt.harass! 
+      debt.last_harassed = time_now
+      debt.save!
+    end
   end
 end
 
 def send_mail?(deadline, last_harassed, angerlevel='polite')
-  puts deadline
-  puts last_harassed
-  puts angerlevel 
-  puts '--'
   return true unless last_harassed
   days_since(last_harassed) > harassment_delay(days_to(deadline), angerlevel)
 end
 
 def harassment_delay(days_remaining, angerlevel='polite')
   case days_remaining
-  when 0..3 then return ANGER_CHART[angerlevel][0]
+  when 0..3 then ANGER_CHART[angerlevel][0]
   when 4..7 then ANGER_CHART[angerlevel][1]
-  when 8..29 then return ANGER_CHART[angerlevel][2]
+  when 8..29 then ANGER_CHART[angerlevel][2]
   when 30..59 then ANGER_CHART[angerlevel][3]
   when 60..120 then ANGER_CHART[angerlevel][4]
   else 30
