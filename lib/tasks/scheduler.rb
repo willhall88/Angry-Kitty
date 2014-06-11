de08a6ca24507a76a0e01eabdc7075e8bbd8c520
Will Hall <willhall88@hotmail.com>
2014-06-11 13:41:56 +0100

    refactored setting of the maturity and elapsed time variables

--- lib/tasks/scheduler.rb
+++ lib/tasks/scheduler.rb
@@ -16,10 +16,10 @@ def send_mail?(deadline,last_harassed)
 
   return true if last_harassed == nil 
 
-  maturity = seconds_to_days((deadline - DateTime.now.in_time_zone).to_f)
-  elapsed_time = seconds_to_days((DateTime.now.in_time_zone - last_harassed).to_f) 
+  maturity = seconds_to_days(deadline - time_now)
+  elapsed_time = seconds_to_days(time_now - last_harassed) 
 
-  return true if maturity > MATURITY_HASH.keys.last && elapsed_time > MATURITY_HASH[121]
+  return true if maturity > MATURITY_HASH.keys.last && elapsed_time > MATURITY_HASH.values.last
 
   MATURITY_HASH.each do |key, value|
     return true if (maturity <= key && elapsed_time > value)  
@@ -32,3 +32,6 @@ def seconds_to_days(time)
   time / 24 / 60 / 60
 end
 
+def time_now
+  DateTime.now.in_time_zone
+end
