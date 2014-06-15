e2c3a2daa2e4bd0a856ee245ef8a073c972ce05d
Nico Saueressig <nico@nicosaueressig.de>
2014-06-15 13:05:46 +0100

    filthy bootstrap, kinda working

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(version: 20140613143623) do
     t.integer "userinvitee_id", null: false
   end
 
+  create_table "events_users", id: false, force: true do |t|
+    t.integer "event_id", null: false
+    t.integer "user_id",  null: false
+  end
+
   create_table "payments", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -57,6 +62,11 @@ ActiveRecord::Schema.define(version: 20140613143623) do
     t.string   "name"
     t.string   "mobile"
     t.integer  "payment_amount"
+  end
+
+  create_table "userinvites", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
