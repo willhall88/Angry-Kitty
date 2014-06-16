854041edcb483d16f247383b32bb85ae7ece3076
DLDB <dpbergquist@gmail.com>
2014-06-16 16:08:29 +0100

    added some functionality to the event show page

---
+++ features/event_view.feature
@@ -0,0 +1,23 @@
+Feature: viewing an event
+  In order to see the payment statuses of event participants
+  As a user
+  I want to see the events page
+
+  Background:
+    Given I am a user
+    And I make an event
+
+  Scenario: an organiser viewing the event
+    When I am on the event page 
+    Then I see "not accepted"
+    
+  Scenario: an organiser viewing an event with 1 unpaid user
+    And the event has 1 participant who has not paid
+    When I am on the event page 
+    Then I want to see that the participant has not paid
+
+  Scenario: an organiser viewing an event with 1 paid user and 1 invitee
+    And the event has 1 participant who has paid
+    When I am on the event page 
+    Then I see that the participant has paid
+    Then I see "not accepted"
---
+++ features/step_definitions/event_view_steps.rb
@@ -0,0 +1,42 @@
+ Given(/^I am a user$/) do
+  @user = create(:user)
+  login_as @user
+end
+
+Given(/^I make an event$/) do
+  @invitee = create(:userinvitee)
+  @event = build(:event, organiser: @user)
+  @event.userinvitees << @invitee
+  @event.save
+end
+
+Then(/^I see "(.*?)"$/) do |arg1|
+  expect(page).to have_content(arg1)
+end
+
+Given(/^the event has (\d+) participant who has not paid$/) do |arg1|
+  @unpaid_ppt = create(:user, email: 'unpaid@user.com')
+  @event.users << @unpaid_ppt
+  @event.save
+end
+
+Then(/^I want to see that the participant has not paid$/) do
+  expect(page).to have_content("| ✗ |")
+end
+
+Given(/^the event has (\d+) participant who has paid$/) do |arg1|
+  @paid_ppt = create(:user, email: 'paid@user.com')
+  @event.users << @paid_ppt
+  @event.save
+  @paid_ppt.debts.last.update(paid: true)
+end
+
+Then(/^I see that the participant has paid$/) do
+  expect(page).to have_content("| ✓ |")
+end
+
+Given(/^the event has (\d+) invitee who has not accepted$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+
