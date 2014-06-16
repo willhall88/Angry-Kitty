da5b2ed745bdd4a18bdd5e17452f48345bc58f97
Nico Saueressig <nico@nicosaueressig.de>
2014-06-16 11:35:04 +0100

    tab for event page, thanks to norwegian black metal band gorgoroth

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,20 +1,20 @@
+$(document).ready(function() {
+    $('.add_userinvitee').on('click', function() {
 
+        var template = $('#userinvitee_fields');
+        var list_length = $('.userinvitees_list li').length;
+        console.log(list_length);
+        var new_invitee_template = Mustache.render(template.html(), {
+            index: list_length
+        });
 
-$(document).ready(function(){
-	$('.add_userinvitee').on('click',function() {
+        $('.userinvitees_list').append('<li>' + new_invitee_template + '</li>');
 
-		var template = $('#userinvitee_fields');
-		var list_length = $('.userinvitees_list li').length;
-		console.log(list_length);
-		var new_invitee_template = Mustache.render(template.html(), {index: list_length});
-	
-		$('.userinvitees_list').append('<li>' + new_invitee_template + '</li>');
-	
-	});
+    });
 
-	$('.userinvitees_list').on('click', '.delete_userinvitee', function() {
-		$(this).parent().remove();
-	});
+    $('.userinvitees_list').on('click', '.delete_userinvitee', function() {
+        $(this).parent().remove();
+    });
 
 
 });
