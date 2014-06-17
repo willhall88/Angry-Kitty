20522206c1db846465e7927b462f2c8361c59ea0
Nico Saueressig <nico@nicosaueressig.de>
2014-06-17 10:35:34 +0100

    moved fonts into vendor folder

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 
 module AngryKitty
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
