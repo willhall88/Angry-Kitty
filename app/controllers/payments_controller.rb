35a2257bb315d45272de870cd344b35a8be64c6e
Nico Saueressig <nico@nicosaueressig.de>
2014-06-17 23:52:30 +0100

    sroop eagle eyes sunar made this fix happen, one new error in cucumber remains

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,14 +3,14 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-  	@event = Event.find(params[:event_id])
-  	user = User.find(params[:user_id])
-  	debt = user.debts.find_by(event_id: params[:event_id])
-  	debt.pay!(current_user)
+    @event = Event.find(params[:event_id])
+    user = User.find(params[:user_id])
+    debt = user.debts.find_by(event_id: params[:event_id])
+    debt.pay!(current_user)
   rescue ActiveRecord::RecordInvalid
-  	flash[:notice] = 'Could not update paid status'
+    flash[:notice] = 'Could not update paid status'
   ensure
-	redirect_to event_path(@event)
+    redirect_to event_path(@event)
   end
 
 end
