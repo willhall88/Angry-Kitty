cc6091b1451c186e3e4d68958959832c1e770fb5
Sroop <sroopsunar@hotmail.com>
2014-06-20 12:38:04 +0100

    Allow mail domain to be specified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: "angrykitty.co.uk" }
+  config.action_mailer.default_url_options = { host: ENV["MAIL_DOMAIN"] || "angrykitty.co.uk" }
 end
