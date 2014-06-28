0feeba6b1e5d74c187d2f9dde9edd405ae6ec219
Sroop Sunar <sroop@users.noreply.github.com>
2014-06-28 01:17:34 +0100

    reworded some lines and tweaked some styling

--- app/assets/javascripts/landingpage.js
+++ app/assets/javascripts/landingpage.js
@@ -2,33 +2,36 @@ $(document).ready(function() {
   $('.nextlink').click(function(event) {
     event.preventDefault();
       
-    $('.text').text("Let me harass them and collect the money for you. The longer they take the angrier I get and the more I chase!");
+    $('.text').text("Let me harass them and collect the money for you. From polite weekly reminders to full blown angry hourly harassment… the longer they take, the angrier I get and the harder I chase!");
 
 
     $('.nextlink').click(function(event) {
-      $('.text').text("You can see who has paid and who hasn't and harass them with me.");
+      $('.text').text("Your friends will find it super easy to pay you back through my simple and secure payment system and you can easily keep track of who has paid and who hasn't (you can even name and shame them on social media if you like).");
 
+       $('.nextlink').click(function(event) {
+         $('.text').text("To set up a kitty, specify how much money you need to collect, the date you need it by, and the contact details of your friends. Leave the nagging to me.");
 
-      $('.nextlink').click(function(event) {
-        $('.nextlink').remove();
-        $('.yeslink').css('visibility', 'visible');
-        $('.nolink').css('visibility', 'visible');
-        $('.text').text("Angry Kitty loves chasing people for money, would you consider letting Angry Kitty do this for you?");
+        $('.nextlink').click(function(event) {
+          $('.nextlink').remove();
+          $('.yeslink').css('visibility', 'visible');
+          $('.nolink').css('visibility', 'visible');
+          $('.text').text("Liking this so far?");
 
-        $('.yeslink').click(function(event) {
-          $('.text').text('Register for the newsletter');
-          $('.yeslink').remove();
-          $('.nolink').remove();
-        });
+          $('.yeslink').click(function(event) {
+            $('.text').text('Awesome! Register your interest.');
+            $('.yeslink').remove();
+            $('.nolink').remove();
+          });
 
-        $('.nolink').click(function(event) {
-          $('.text').text('Thanks for your time');
-          $('.yeslink').remove();
-          $('.nolink').remove();
+          $('.nolink').click(function(event) {
+            $('.text').text('Ok fine.');
+            $('.yeslink').remove();
+            $('.nolink').remove();
+          });
+
         });
 
       });
-
     });
 
    });
